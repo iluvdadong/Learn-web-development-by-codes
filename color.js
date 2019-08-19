4c9256a322950bbe25fab88d91bda85c3beee997
Dahae Kim <dh77dh77@gmail.com>
2019-08-19 14:09:33 +0900

    Ajax 실습환경 준비

--- color.js
+++ color.js
@@ -1,24 +1,37 @@
-function nightDayHandler(self) {
+var Links = {
+  setColor:function(color){
+    // var alist = document.querySelectorAll('a');
+    // var i = 0;
+    // while(i < alist.length){
+    //   alist[i].style.color = color;
+    //   i = i + 1;
+    // }
+    $('a').css('color', color);
+  }
+}
+var Body = {
+  setColor:function (color){
+    //document.querySelector('body').style.color = color;
+    $('body').css('color', color);
+  },
+  setBackgroundColor:function (color){
+    // document.querySelector('body').style.backgroundColor = color;
+    $('body').css('backgroundColor', color);
+  }
+}
+function nightDayHandler(self){
   var target = document.querySelector('body');
-  if(self.value === 'night') {
-    target.style.backgroundColor='black'; target.style.color='yellow';
+  if(self.value === 'night'){
+    Body.setBackgroundColor('black');
+    Body.setColor('white');
     self.value = 'day';
 
-    var alist = document.querySelectorAll('a');
-    var i = 0;
-    while(i < alist.length) {
-      alist[i].style.color = 'powderblue';
-      i = i+1;
-    }
+    Links.setColor('white');
   } else {
-    target.style.backgroundColor='white'; target.style.color='black';
+    Body.setBackgroundColor('white');
+    Body.setColor('black');
     self.value = 'night';
 
-    var alist = document.querySelectorAll('a');
-    var i = 0;
-    while(i < alist.length) {
-      alist[i].style.color = 'blue';
-      i = i+1;
-    }
+    Links.setColor('blue');
   }
 }
